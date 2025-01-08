5137055da7e6df7358a4ff834a306eb4ac860830
Lucy Harris <lucyalisonharris@gmail.com>
2025-01-08 18:00:27 +0100

    new condition behaviours

--- heron_planner/scripts/arm_behaviours.py
+++ heron_planner/scripts/arm_behaviours.py
@@ -4,8 +4,10 @@ import rospy
 import actionlib
 import py_trees as pt
 
+import heron_utils.transform_utils as utils
+
 from geometry_msgs.msg import PoseStamped
-from std_msgs.msg import Header
+from std_msgs.msg import String, Header
 
 from robot_simple_command_manager_msgs.srv import SetCommandString
 from heron_msgs.msg import (
@@ -14,6 +16,40 @@ from heron_msgs.msg import (
     PickupFromAction, PickupFromGoal,
     PlaceOnAction, PlaceOnGoal,
 )
+
+
+class ArmAt(pt.behaviour.Behaviour):    
+    def __init__(self, name: str, pose_name: str) -> None:
+        super().__init__(name)
+        self.goal_pose_name = pose_name
+        self.current_pose_name = None
+
+        self.recieved_name = False
+        self.at_pose = None
+
+        rospy.Subscriber("/robot/arm/ee_pose_name", String, self.pose_name_cb)
+
+    def pose_name_cb(self, msg: String) -> None:        
+        self.current_pose_name = msg
+        self.recieved_name = True
+
+    def initialise(self):
+        if self.recieved_name is True:
+            if self.current_pose_name.data == self.goal_pose_name:
+                self.at_pose = True
+            else:
+                self.at_pose = False
+
+    def update(self) -> pt.common.Status:
+
+        if not self.at_pose:
+            rospy.loginfo(f"Failed: not at GPS location")
+            return pt.common.Status.FAILURE
+        elif self.at_pose:
+            rospy.loginfo("Success, at GPS location")
+            return pt.common.Status.SUCCESS
+        return pt.common.Status.FAILURE
+
 
 class MoveTo(pt.behaviour.Behaviour):
     def __init__(self, name: str, pose_name: str) -> None:
@@ -24,10 +60,10 @@ class MoveTo(pt.behaviour.Behaviour):
         self._result = None
         self._feedback_recieved = None
         
-    def setup(self, timeout: float = 2.0) -> bool:
+    def setup(self) -> bool:
         try:
             self._client = actionlib.SimpleActionClient(self._node_name, MoveToAction)
-            if not self._client.wait_for_server(timeout=timeout):
+            if not self._client.wait_for_server(timeout=rospy.Duration(2)):
                 rospy.logerr(f"Action {self._node_name} setup failed: {err}")
                 return False
             rospy.loginfo(f"Action {self._node_name} is ready")
@@ -35,6 +71,7 @@ class MoveTo(pt.behaviour.Behaviour):
         except rospy.ROSException as err:
             rospy.logerr(f"Action {self._node_name} setup failed: {err}")
             return False
+    
         
     def initialise(self):
         
@@ -75,6 +112,35 @@ class MoveTo(pt.behaviour.Behaviour):
         rospy.loginfo(f"Feedback: {feedback}")
         self._feedback_recieved = True
         
+
+class ArmAtPose(pt.behaviour.Behaviour):    
+    def __init__(self, name: str, pose: PoseStamped) -> None:
+        super().__init__(name)
+        self.goal_pose = pose
+        self.current_pose = None
+
+        self.recieved_pose = False
+        self.at_pose = None
+
+        rospy.Subscriber("/robot/arm/ee_pose", PoseStamped, self.pose_cb)
+
+    def pose_cb(self, msg: PoseStamped) -> None:        
+        self.current_pose = msg
+        self.recieved_pose = True
+
+    def initialise(self):
+        if self.recieved_pose is True:
+            self.at_gps = utils.at_pose(self.current_pose.pose, self.goal_pose.pose) 
+
+    def update(self) -> pt.common.Status:
+
+        if not self.at_gps:
+            rospy.loginfo(f"Failed: not at GPS location")
+            return pt.common.Status.FAILURE
+        elif self.at_gps:
+            rospy.loginfo("Success, at GPS location")
+            return pt.common.Status.SUCCESS
+        return pt.common.Status.FAILURE
 
 class MoveToPose(pt.behaviour.Behaviour):
     def __init__(self, name: str, pose: PoseStamped) -> None:
--- heron_planner/scripts/base_behaviours.py
+++ heron_planner/scripts/base_behaviours.py
@@ -5,14 +5,23 @@ import py_trees as pt
 from typing import List, Any
 import numpy as np
 
+import heron_utils.transform_utils as utils
+
+from sensor_msgs.msg import NavSatFix
+
 from robot_simple_command_manager_msgs.srv import SetCommandString
-from heron_msgs.srv import ChangeRobotMode, ChangeRobotModeRequest, ChangeRobotModeResponse
+from heron_msgs.srv import (
+    ChangeRobotMode,
+    ChangeRobotModeRequest,
+    ChangeRobotModeResponse,
+)
+
 
 class ROSWait(pt.behaviour.Behaviour):
     def __init__(self, name: str, wait_time: float) -> None:
         super().__init__(name)
         self._wait_time = wait_time
-        
+
     def initialise(self):
         self._rate = rospy.Rate(1.0 / self._wait_time)
 
@@ -27,7 +36,7 @@ class Wait(pt.behaviour.Behaviour):
         super().__init__(name)
         self._srv_name = "/robot/command_manager/command"
         self._req = "WAIT " + wait_time
-        
+
     def setup(self, timeout: float = 2.0) -> bool:
         try:
             self._client = rospy.ServiceProxy(self._srv_name, SetCommandString)
@@ -35,7 +44,7 @@ class Wait(pt.behaviour.Behaviour):
             return True
         except rospy.ROSException as err:
             rospy.logerr(f"Service {self._srv_name} setup failed: {err}")
-        
+
     def initialise(self):
         self._srv_setup = False
         self._srv_called = False
@@ -63,13 +72,14 @@ class Wait(pt.behaviour.Behaviour):
             rospy.loginfo("Success")
             return pt.common.Status.SUCCESS
         return pt.common.Status.FAILURE
+
 
 class Move(pt.behaviour.Behaviour):
     def __init__(self, name: str, x_dir: str, y_dir: str) -> None:
         super().__init__(name)
         self._srv_name = "/robot/command_manager/command"
         self._req = "MOVE " + x_dir + " " + y_dir
-        
+
     def setup(self, timeout: float = 2.0) -> bool:
         try:
             self._client = rospy.ServiceProxy(self._srv_name, SetCommandString)
@@ -77,7 +87,7 @@ class Move(pt.behaviour.Behaviour):
             return True
         except rospy.ROSException as err:
             rospy.logerr(f"Service {self._srv_name} setup failed: {err}")
-        
+
     def initialise(self):
         self._srv_setup = False
         self._srv_called = False
@@ -105,7 +115,6 @@ class Move(pt.behaviour.Behaviour):
             rospy.loginfo("Success")
             return pt.common.Status.SUCCESS
         return pt.common.Status.FAILURE
-
 
 
 class Turn(pt.behaviour.Behaviour):
@@ -113,7 +122,7 @@ class Turn(pt.behaviour.Behaviour):
         super().__init__(name)
         self._srv_name = "/robot/command_manager/command"
         self._req = "TURN " + angle
-        
+
     def setup(self, timeout: float = 2.0) -> bool:
         try:
             self._client = rospy.ServiceProxy(self._srv_name, SetCommandString)
@@ -121,7 +130,7 @@ class Turn(pt.behaviour.Behaviour):
             return True
         except rospy.ROSException as err:
             rospy.logerr(f"Service {self._srv_name} setup failed: {err}")
-        
+
     def initialise(self):
         self._srv_setup = False
         self._srv_called = False
@@ -132,6 +141,81 @@ class Turn(pt.behaviour.Behaviour):
         if not self._srv_setup:
             rospy.logwarn(f"Service {self._srv_name} failed")
             return
+        try:
+            rospy.loginfo(f"Sending command {self._req}")
+            self._res = self._client(self._req)
+            self._srv_called = True
+        except rospy.ServiceException as err:
+            rospy.logerr(f"Service {self._srv_name} failed : {err}")
+            self._srv_called = False
+
+    def update(self) -> pt.common.Status:
+
+        if not self._srv_setup or not self._srv_called:
+            rospy.loginfo(f"Failed: {self._srv_setup} and {self._srv_called}")
+            return pt.common.Status.FAILURE
+        if self._res and self._res.ret.success:
+            rospy.loginfo("Success")
+            return pt.common.Status.SUCCESS
+        return pt.common.Status.FAILURE
+
+
+class AtGPS(pt.behaviour.Behaviour):
+    def __init__(self, name: str, goal_gps: NavSatFix) -> None:
+        super().__init__(name)
+        self.goal_gps = goal_gps
+        self.current_gps = None
+
+        self.recieved_gps = False
+        self.at_gps = None
+
+        rospy.Subscriber("/robot/gps/fix", NavSatFix, self.gps_cb)
+
+    def gps_cb(self, msg: NavSatFix) -> None:        
+        self.current_gps = msg
+        self.recieved_gps = True
+
+    def initialise(self):
+        if self.recieved_gps is True:
+            self.at_gps = utils.at_gps(self.current_gps, self.goal_gps) 
+
+    def update(self) -> pt.common.Status:
+
+        if not self.at_gps:
+            rospy.loginfo(f"Failed: not at GPS location")
+            return pt.common.Status.FAILURE
+        elif self.at_gps:
+            rospy.loginfo("Success, at GPS location")
+            return pt.common.Status.SUCCESS
+        return pt.common.Status.FAILURE
+
+
+class GoToGPS(pt.behaviour.Behaviour):
+    def __init__(self, name: str, gps: NavSatFix):
+        super().__init__(name)
+        self._srv_name = "/robot/command_manager/command"
+        #TODO define this w/ Raquel
+        yaw = str(0.0)
+        self._req = "GOTO_GPS " + str(gps.latitude) + " " + str(gps.longitude) + " " + yaw
+
+    def setup(self, timeout: float = 2.0) -> bool:
+        try:
+            self._client = rospy.ServiceProxy(self._srv_name, SetCommandString)
+            rospy.wait_for_service(self._srv_name, timeout=timeout)
+            return True
+        except rospy.ROSException as err:
+            rospy.logerr(f"Service {self._srv_name} setup failed: {err}")
+
+    def initialise(self):
+        self._srv_setup = False
+        self._srv_called = False
+        self._res = None
+
+        self._srv_setup = self.setup()
+        if not self._srv_setup:
+            rospy.logwarn(f"Service {self._srv_name} failed")
+            return
+
         try:
             rospy.loginfo(f"Sending command {self._req}")
             self._res = self._client(self._req)
@@ -156,7 +240,7 @@ class Dock(pt.behaviour.Behaviour):
         super().__init__(name)
         self._srv_name = "/robot/command_manager/command"
         self._req = "DOCK"
-        
+
     def setup(self, timeout: float = 2.0) -> bool:
         try:
             self._client = rospy.ServiceProxy(self._srv_name, SetCommandString)
@@ -164,7 +248,7 @@ class Dock(pt.behaviour.Behaviour):
             return True
         except rospy.ROSException as err:
             rospy.logerr(f"Service {self._srv_name} setup failed: {err}")
-        
+
     def initialise(self):
         self._srv_setup = False
         self._srv_called = False
@@ -184,7 +268,7 @@ class Dock(pt.behaviour.Behaviour):
             self._srv_called = False
 
     def update(self) -> pt.common.Status:
-        
+
         if not self._srv_setup or not self._srv_called:
             rospy.loginfo(f"Failed: {self._srv_setup} and {self._srv_called}")
             return pt.common.Status.FAILURE
@@ -200,7 +284,7 @@ class ChangeRobotMode(pt.behaviour.Behaviour):
         self._srv_name = "/robot/change_mode"
         self._req = ChangeRobotModeRequest(mode=change_req)
         self._res = ChangeRobotModeResponse()
-        
+
     def setup(self, timeout: float = 2.0) -> bool:
         try:
             self._client = rospy.ServiceProxy(self._srv_name, ChangeRobotMode)
@@ -208,7 +292,7 @@ class ChangeRobotMode(pt.behaviour.Behaviour):
             return True
         except rospy.ROSException as err:
             rospy.logerr(f"Service {self._srv_name} setup failed: {err}")
-        
+
     def initialise(self):
         self._srv_setup = False
         self._srv_called = False
