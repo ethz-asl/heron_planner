17055da35648746e41ad8e72e32011c284e4c88e
Lucy Harris <lucyalisonharris@gmail.com>
2025-01-21 17:20:53 +0100

    adding fake TF links

--- dummy_interface/src/dummy_interface/fake_data_publisher.py
+++ dummy_interface/src/dummy_interface/fake_data_publisher.py
@@ -2,9 +2,11 @@
 
 import rospy
 from robotnik_msgs.msg import BatteryStatus, State
-from sensor_msgs.msg import NavSatFix, Image
+from sensor_msgs.msg import NavSatFix, Image, NavSatStatus
 from std_msgs.msg import String, Header
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, TransformStamped
+import tf2_ros
+import tf_conversions
 import random
 import numpy as np
 from cv_bridge import CvBridge
@@ -46,10 +48,46 @@ class FakeDataPublisher:
             Image,
             queue_size=10,
         )
+        self.hlp_state_pub = rospy.Publisher("/hlp/state", String, queue_size=10)
+
+        # create transforms for TF
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster()
+
+        self.transforms = [
+            self.create_transform("odom", "base_link", 0, 0, 0),
+            self.create_transform("base_link", "base_camera_link", 0.2, 0.0, 0.5),
+            self.create_transform("base_camera_link", "arm_camera_link", 0.1, 0.0, 0.2),
+        ]
+
 
         self.bridge = CvBridge()
 
         rospy.loginfo("Fake data publisher initialized.")
+
+    
+    def create_transform(self, parent_frame, child_frame, x, y, z, roll=0, pitch=0, yaw=0):
+        """
+        create a TransformStamped object for broadcasting.
+        #TODO move to utils
+        """
+        transform = TransformStamped()
+        transform.header.frame_id = parent_frame
+        transform.child_frame_id = child_frame
+        transform.header.stamp = rospy.Time.now()
+
+        # set translation
+        transform.transform.translation.x = x
+        transform.transform.translation.y = y
+        transform.transform.translation.z = z
+
+        # set rotation (quaternion)
+        quat = tf_conversions.transformations.quaternion_from_euler(roll, pitch, yaw)
+        transform.transform.rotation.x = quat[0]
+        transform.transform.rotation.y = quat[1]
+        transform.transform.rotation.z = quat[2]
+        transform.transform.rotation.w = quat[3]
+
+        return transform
 
     def publish_battery_status(self):
         """Publishes a faked battery status."""
@@ -77,10 +115,13 @@ class FakeDataPublisher:
     def publish_gps_data(self):
         """Publishes faked GPS coordinates."""
         gps_msg = NavSatFix()
-        gps_msg.header = Header(stamp=rospy.Time.now(), frame_id="base_link")
-        gps_msg.latitude = random.uniform(-90.0, 90.0)  # Random latitude
-        gps_msg.longitude = random.uniform(-180.0, 180.0)  # Random longitude
-        gps_msg.altitude = random.uniform(0.0, 1000.0)  # Altitude in meters
+        gps_msg.latitude = round(random.uniform(-90.0, 90.0), 6)
+        gps_msg.longitude = round(random.uniform(-180.0, 180.0), 6)
+        gps_msg.altitude = round(random.uniform(-50.0, 8848.0), 2)  # Altitude in meters
+        gps_msg.position_covariance = [0.5] * 9  # Fake fixed covariance matrix
+        gps_msg.position_covariance_type = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+        gps_msg.status.status = NavSatStatus.STATUS_FIX  # Assume a valid fix
+        gps_msg.status.service = NavSatStatus.SERVICE_GPS
         self.gps_pub.publish(gps_msg)
         rospy.loginfo(f"Published fake GPS data: {gps_msg}")
 
@@ -147,17 +188,44 @@ class FakeDataPublisher:
         pub.publish(ros_image)
         rospy.loginfo("Published a fake camera image.")
 
+    def publish_hlp_state(self):
+        """
+        Generate a fake high-level planning (HLP) state as a string.
+        """
+        hlp_states = [
+            "[-] Before inspection [*]\n[o] PotholeOffsetSelector [✓]",
+            "[o] HomeSelector [*] --> Arm at home? [✕]",
+            "[o] Inspect and find pothole\n-^- Retry Inspection Locations",
+            "[o] Search Inspection Location [-] --> Find pothole",
+            "[o] No Pothole Found --> Send pothole segmentation to Kafka",
+        ]
+        hlp_msg = String(data=(random.choice(hlp_states)))
+        self.hlp_state_pub.publish(hlp_msg)
+        rospy.loginfo("Published a fake hlp state.")
+
+    def publish_transforms(self):
+        """
+        publish static transforms to the tf tree.
+        """
+        for transform in self.transforms:
+            transform.header.stamp = rospy.Time.now()  
+            self.tf_broadcaster.sendTransform(transform)
+
+        rospy.loginfo("Publsihed fake transforms")
+
     def run(self):
         rate = rospy.Rate(0.1)  # Publish at 0.1 Hz
         while not rospy.is_shutdown():
             self.publish_battery_status()
-            # self.publish_gps_data()
-            self.publish_consistent_gps_data()
+            self.publish_gps_data()
+            # self.publish_consistent_gps_data()
             self.publish_robot_status()
-            self.publish_ee_pose()
-            self.publish_ee_pose_name()
-            self.publish_camera_image(self.arm_camera_pub)
-            self.publish_camera_image(self.body_camera_pub)
+            self.publish_hlp_state()
+            self.publish_transforms()
+            # self.publish_ee_pose()
+            # self.publish_ee_pose_name()
+            # self.publish_camera_image(self.arm_camera_pub)
+            # self.publish_camera_image(self.body_camera_pub)
             rate.sleep()
 
 
