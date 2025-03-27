1f2be3fb674d1fd06a1213164f6b217a487850c2
Lucy Harris <lucyalisonharris@gmail.com>
2025-03-27 15:23:08 +0100

    pothole test

--- heron_planner/scripts/pothole_bt.py
+++ heron_planner/scripts/pothole_bt.py
@@ -60,15 +60,13 @@ class PotholeBT(base_bt.BaseBT):
             image_key=img_key,
             save=True,
         )
-        if rospy.get_param("/kafka", False):
-            send_img_to_kafka = hlp.FakeSendImageToKafka(
-                task_name=send_kafka_task_name,
-                msg=kafka_msg,
-                load_key=img_key
-            )
+        # if rospy.get_param("/kafka", False):
+        send_img_to_kafka = hlp.FakeSendImageToKafka(
+            task_name=send_kafka_task_name,
+            msg=kafka_msg,
+            load_key=img_key
+        )
 
-        else: 
-            pass
             # send_img_to_kafka = hlp.SendImageToKafka(
             #     task_name=send_kafka_task_name,
             #     msg=kafka_msg,
